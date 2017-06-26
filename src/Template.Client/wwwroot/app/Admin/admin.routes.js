64207c836722f4ef244d7f5b21380d2a70e13c65
Vilmantas <vilmantas@mediapark.lt>
2017-06-26 15:13:16 +0300

    wtf

--- src/Template.Client/wwwroot/app/Admin/admin.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var admin_internal_component_1 = require("./internal/admin.internal.component");
-var admin_public_component_1 = require("./public/admin.public.component");
-exports.adminRoutes = [
-    { path: 'internal', component: admin_internal_component_1.AdminInternalComponent, loadChildren: 'app/admin/internal/admin.internal.module#AdminInternalModule' },
-    { path: '', component: admin_public_component_1.AdminpublicComponent, loadChildren: 'app/admin/public/admin.public.module#AdminPublicModule' }
-];
-//# sourceMappingURL=admin.routes.js.map
--- src/Template.Client/wwwroot/app/Client/internal/client.internal.routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clientInternalRoutes = [
-    {}
-];
-//# sourceMappingURL=client.internal.routes.js.map
