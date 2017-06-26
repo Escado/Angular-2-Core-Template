f2e16d3d22a494d47f3625520f5b7bb371a858b7
Vilmantas <vilmantas@mediapark.lt>
2017-06-26 15:15:25 +0300

    wtf v3

--- src/Template.Client/wwwroot/app/Admin/public/admin.public.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var login_component_1 = require("./login/login.component");
-exports.adminPublicRoutes = [
-    { path: '', component: login_component_1.LoginComponent },
-    { path: '**', redirectTo: '' }
-];
-//# sourceMappingURL=admin.public.routes.js.map
