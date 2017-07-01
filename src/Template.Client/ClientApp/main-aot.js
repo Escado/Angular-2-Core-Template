3a6bbe0693d4e83a527b8b23159d8d7082ec60bd
Vilmantas <escado1@gmail.com>
2017-07-01 20:51:10 +0300

    removed js

--- src/Template.Client/ClientApp/main-aot.js
+++
@@ -1,5 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from '../aot/ClientApp/app/application.module.ngfactory';
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
